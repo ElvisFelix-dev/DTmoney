5fd4e463b83a1b8a77c604d857778a8dfae475c0
Elvis Felix <elvisjaspion@gmail.com>
2023-02-09 13:36:51 -0300

    update - add cards

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@ import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
 import { Transactions } from './pages/Transactions'
-import { Summary } from './components/Summary'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <Transactions />
-      <Summary />
       <GlobalStyle />
     </ThemeProvider>
   )
