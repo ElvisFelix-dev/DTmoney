77b3282e9fb133b85fd5ba7b5a8766fedd8a1a5e
Elvis Felix <elvisjaspion@gmail.com>
2023-02-09 15:05:58 -0300

    add table/search

---
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -0,0 +1,20 @@
+import { MagnifyingGlass } from 'phosphor-react'
+import { SearchFormContainer } from './styles'
+
+export function SearchForm() {
+  return (
+    <SearchFormContainer>
+      <input
+        type="text"
+        placeholder="Busque por transações"
+        title="Digite uma transação para busca"
+        required
+      />
+
+      <button type="submit">
+        <MagnifyingGlass size={20} />
+        Buscar
+      </button>
+    </SearchFormContainer>
+  )
+}
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,42 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
+import { SearchForm } from './components/SearchForm'
+import {
+  PriceHighLight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles'
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <SearchForm />
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighLight variant="income">R$ 12.000,00</PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+
+            <tr>
+              <td>Burg King</td>
+              <td>
+                <PriceHighLight variant="outcome"> - R$ 58,00</PriceHighLight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/02/2023</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
