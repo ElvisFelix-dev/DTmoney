4ce974cabbaa50ea4c2d80b3d5424d0455c97fc4
Elvis Felix <elvisjaspion@gmail.com>
2023-02-10 23:10:34 -0300

    add footer

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,3 +1,4 @@
+import { Footer } from '../../components/Footer'
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
 import { SearchForm } from './components/SearchForm'
@@ -36,16 +37,52 @@ export function Transactions() {
             </tr>
 
             <tr>
-              <td>Burg King</td>
+              <td>Condominio</td>
               <td>
-                <PriceHighLight variant="outcome"> - R$ 58,00</PriceHighLight>
+                <PriceHighLight variant="outcome"> - R$ 200,00</PriceHighLight>
               </td>
-              <td>Alimentação</td>
+              <td>Moradia</td>
+              <td>10/02/2023</td>
+            </tr>
+
+            <tr>
+              <td>Internet</td>
+              <td>
+                <PriceHighLight variant="outcome"> - R$ 90,00</PriceHighLight>
+              </td>
+              <td>Contas</td>
+              <td>10/02/2023</td>
+            </tr>
+
+            <tr>
+              <td>Aluguel da plataforma</td>
+              <td>
+                <PriceHighLight variant="income">R$ 100,00</PriceHighLight>
+              </td>
+              <td>Trabalhos</td>
+              <td>10/02/2023</td>
+            </tr>
+
+            <tr>
+              <td>Notebook</td>
+              <td>
+                <PriceHighLight variant="outcome"> - R$ 200,00</PriceHighLight>
+              </td>
+              <td>Acessorios</td>
+              <td>10/02/2023</td>
+            </tr>
+            <tr>
+              <td>Carro</td>
+              <td>
+                <PriceHighLight variant="income">R$ 10.000,00</PriceHighLight>
+              </td>
+              <td>Venda</td>
               <td>10/02/2023</td>
             </tr>
           </tbody>
         </TransactionsTable>
       </TransactionsContainer>
+      <Footer />
     </div>
   )
 }
