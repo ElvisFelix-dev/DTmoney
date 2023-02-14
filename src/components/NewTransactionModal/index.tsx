b41a2a2d912f8a1e7ff9d5d5ea20ca8a863a4513
Elvis Felix <elvisjaspion@gmail.com>
2023-02-14 12:38:43 -0200

    add hook useMemo

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,9 +1,9 @@
-import { useContext } from 'react'
 import { Controller, useForm } from 'react-hook-form'
 import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
 import * as Dialog from '@radix-ui/react-dialog'
 import * as z from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { useContextSelector } from 'use-context-selector'
 
 import { TransactionsContext } from '../../contexts/TransactionsContext'
 
@@ -25,7 +25,12 @@ const newTransactionFormSchema = z.object({
 type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>
 
 export function NewTransactionModal() {
-  const { createTransaction } = useContext(TransactionsContext)
+  const createTransaction = useContextSelector(
+    TransactionsContext,
+    (context) => {
+      return context.createTransaction
+    },
+  )
 
   const {
     control,
--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,8 +1,10 @@
-import { useContext } from 'react'
+// import { memo } from 'react'
+import { useContextSelector } from 'use-context-selector'
 import { MagnifyingGlass } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import * as z from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
+
 import { TransactionsContext } from '../../../../contexts/TransactionsContext'
 
 import { SearchFormContainer } from './styles'
@@ -14,7 +16,12 @@ const searchFormSchema = z.object({
 type SearchFormInputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm() {
-  const { fetchTransactions } = useContext(TransactionsContext)
+  const fetchTransactions = useContextSelector(
+    TransactionsContext,
+    (context) => {
+      return context.fetchTransactions
+    },
+  )
 
   const {
     register,
@@ -49,3 +56,6 @@ export function SearchForm() {
     </SearchFormContainer>
   )
 }
+
+// export const SearchForm = memo(SearchFormComponent)
+// A linhas comentadas são para o uso do memo
